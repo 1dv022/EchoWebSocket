627b8e2a1d045090f73ca587fcb32c54809e7485
John Häggerud <john.haggerud@lnu.se>
2015-12-14 08:52:24 +0100

    add comments

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ port: 8888 });
+var wss = new WebSocketServer({ port: 8080 });
 
 var RateLimiter = require('limiter').RateLimiter;
+// allow 60 messages per minute
 var limiter = new RateLimiter(60, 'min', true);
 
 
@@ -11,37 +12,43 @@ var limiter = new RateLimiter(60, 'min', true);
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     limiter.removeTokens(1, function(err, remainingRequests) {
-    //  console.log(err);
+
+      // Notify client if he/she makes to many request
       if (remainingRequests < 0) {
         return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'To many request! Only 60 req / min' }));
-
       }
+
+      // Start handling the request and parse the message
       var clientData;
-        try {
-          clientData = JSON.parse(message);
-        } catch(e) {
-          return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send valid JSON!' }));
-        }
+      try {
+        clientData = JSON.parse(message);
+      } catch(e) {
+        return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send valid JSON!' }));
+      }
 
-        if (clientData.key !== 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd') {
-            return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send a valid key!' }));
-        }
-        else {
-          delete clientData.key;
-        }
-        console.log(clientData);
-        if(clientData.hasOwnProperty('type') && clientData.hasOwnProperty('data') && clientData.hasOwnProperty('username')) {
-          wss.clients.forEach(function each(client) {
-            client.send(JSON.stringify(clientData));
-          });
-        }
-        else {
-          ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send at least properties type, data and username' }));
-        }
+      // check if key is valid
+      if (clientData.key !== 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd') {
+          return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send a valid key!' }));
+      }
+      else {
+        delete clientData.key;
+      }
+
+      // validate the message format
+      if(clientData.hasOwnProperty('type') && clientData.hasOwnProperty('data') && clientData.hasOwnProperty('username')) {
+        // send to all clients
+        wss.clients.forEach(function each(client) {
+          client.send(JSON.stringify(clientData));
+        });
+      }
+      else {
+
+        ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send at least properties type, data and username' }));
+      }
       });
-
-  });
-  ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You are connected!' }));
+    });
+    // return when connected
+    ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You are connected!' }));
 });
 
 /*
