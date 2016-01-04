f9addcd52bd0e1a821112d9470f6aed5907c36f6
John Häggerud <john.haggerud@lnu.se>
2016-01-04 08:16:12 +0100

    Some updates

--- app.js
+++ app.js
@@ -2,41 +2,26 @@
 
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 8080 });
-
-//var RateLimiter = require('limiter').RateLimiter;
-// allow 60 messages per minute
-//var limiter = new RateLimiter(60, 'min', true);
 var limit = require('./limiter');
-var express = require('express');
-var app = express();
-var expressWs = require('express-ws')(app); //app = express app
 
-//app.ws('/socket', function(ws, req) {
-//  console.log("Hit!")
-  //handleWS(ws, limit.isLimited(req));
-//});
-
-
-//function handleWS(ws, isLimitOK) {
-  wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-      // Notify client if he/she makes to many request
-    //  console.log(ws.upgradeReq);
-      if (!limit.isLimited(ws.upgradeReq)) {
-        return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'To many request! Only 60 req / min' }));
-      }
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    var ip = ws.upgradeReq.headers['x-forwarded-for'] || ws.upgradeReq.connection.remoteAddress;
+    if (!limit.isLimited(ip)) {
+      return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'To many request! Only'}));
+    }
 
       // Start handling the request and parse the message
       var clientData;
       try {
         clientData = JSON.parse(message);
       } catch(e) {
-        return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send valid JSON!' }));
+        return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send valid JSON'}));
       }
 
       // check if key is valid
       if (clientData.key !== 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd') {
-          return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send a valid key!' }));
+          return ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send a valid key'}));
       }
       else {
         delete clientData.key;
@@ -50,33 +35,18 @@ var expressWs = require('express-ws')(app); //app = express app
         });
       }
       else {
-        ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send at least properties type, data and username' }));
+        ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You must send at least property type, data and username'}));
       }
     });
     ws.send(JSON.stringify({username: 'The Server', type: 'notification', data: 'You are connected!' }));
   });
-//}
-
-
-
 
 setInterval(function() {
-  console.log('Set intervall');
+  //console.log('Set intervall');
   if(wss.clients && wss.clients.length > 0) {
-    console.log('heartbeat');
+    //console.log('heartbeat');
     wss.clients.forEach(function each(client) {
         client.send(JSON.stringify({username: 'The Server', type: 'heartbeat', data: '' }));
       });
   }
 }, 40000);
-
-
-
-/*
-{
-”type”: ”message”,
-”data” : ”Mitt fina meddelande. Kanske base64-encodat?”
-”username”: ”Kalle Kula”
-”channel” : ”upg3”
-}
-*/
--- limiter.js
+++ limiter.js
@@ -3,20 +3,20 @@
 var RateLimiter = require('limiter').RateLimiter;
 var cache = require('memory-cache');
 
-exports.isLimited = function(req) {
+exports.isLimited = function(ip) {
     var cachedLimiter;
-    if (cache.get(req.ip)) {
-        cachedLimiter = cache.get(req.ip);
+    if (cache.get(ip)) {
+        cachedLimiter = cache.get(ip);
         if (cachedLimiter.getTokensRemaining()>1){
             cachedLimiter.removeTokens(1, function(){});
-            cache.put(req.ip, cachedLimiter, 10000);
+            cache.put(ip, cachedLimiter, 10000);
             return true;
         }
         return false;
     }
     else {
-      cachedLimiter = new RateLimiter(30, 'min');
-      cache.put(req.ip, cachedLimiter, 10000);
+      cachedLimiter = new RateLimiter(3, 'min');
+      cache.put(ip, cachedLimiter, 10000);
       return true;
     }
 };
